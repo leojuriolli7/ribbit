0a689946619cbaac1e8d7b0cd224b195db6cbae0
leojuriolli7 <leojuriolli@gmail.com>
2023-07-09 13:05:21 -0300

    refactor: remove node crypto for random hash

--- src/app/_actions/post.actions.ts
+++ src/app/_actions/post.actions.ts
@@ -3,18 +3,24 @@
 import { db } from "@/db";
 import { posts } from "@/db/schema";
 import slugify from "slugify";
-import crypto from "crypto";
 import type { CreatePostInput } from "@/lib/validations/post.schema";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
-const randomHash = crypto
-  .randomBytes(4)
-  .toString("hex")
-  .substring(4)
-  .toLowerCase();
+function getRandomHash() {
+  const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+
+  for (let i = 0; i < 4; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters[randomIndex];
+  }
+  return result;
+}
 
 export const createPostAction = async (values: CreatePostInput) => {
+  const randomHash = getRandomHash();
+
   const generatedSlug = slugify(values.title, {
     lower: true,
   });
