6ead28fea19c79dac13f6a34f3cb15aab134971d
leojuriolli7 <leojuriolli@gmail.com>
2023-07-11 22:12:59 -0300

    fix: post author skeleton margin

--- src/components/ui/post-author-skeleton.tsx
+++ src/components/ui/post-author-skeleton.tsx
@@ -5,7 +5,7 @@ import { Skeleton } from "./skeleton";
 export const PostAuthorSkeleton = () => {
   return (
     <div className="w-full flex gap-2 items-center">
-      <Skeleton className="rounded-full w-8 h-8" />
+      <Skeleton className="rounded-full w-8 h-8 mt-6" />
       <Skeleton className="w-24 h-5 mt-6" />
     </div>
   );
