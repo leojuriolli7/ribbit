8cf8cfcf91a4bd5b7b79df3180eff80dc8c5caf6
leojuriolli7 <leojuriolli@gmail.com>
2023-07-15 10:55:17 -0300

    fix: improve webhooks route handler code

--- src/app/api/webhooks/user/route.ts
+++ src/app/api/webhooks/user/route.ts
@@ -1,39 +1,44 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-
-import { db } from "@/db";
-import { users } from "@/db/schema";
-import type { IncomingHttpHeaders } from "http";
-import type { User } from "@clerk/nextjs/api";
+import type { User as UserInterface } from "@clerk/nextjs/api";
+import { Webhook } from "svix";
 import { headers } from "next/headers";
-import { NextResponse } from "next/server";
-import { Webhook, type WebhookRequiredHeaders } from "svix";
+import { users } from "@/db/schema";
+import { db } from "@/db";
 import { eq } from "drizzle-orm";
 
-const webhookSecret = process.env.WEBHOOK_SECRET || "";
+const webhookSecret: string = process.env.WEBHOOK_SECRET || "";
 
-async function handler(request: Request) {
-  const payload = await request.json();
-  const headersList = headers();
-  const heads = {
-    "svix-id": headersList.get("svix-id"),
-    "svix-timestamp": headersList.get("svix-timestamp"),
-    "svix-signature": headersList.get("svix-signature"),
+export async function POST(req: Request) {
+  const payload = await req.json();
+  const payloadString = JSON.stringify(payload);
+  const headerPayload = headers();
+  const svixId = headerPayload.get("svix-id");
+  const svixIdTimeStamp = headerPayload.get("svix-timestamp");
+  const svixSignature = headerPayload.get("svix-signature");
+  if (!svixId || !svixIdTimeStamp || !svixSignature) {
+    console.log("svixId", svixId);
+    console.log("svixIdTimeStamp", svixIdTimeStamp);
+    console.log("svixSignature", svixSignature);
+    return new Response("Error occured", {
+      status: 400,
+    });
+  }
+  const svixHeaders = {
+    "svix-id": svixId,
+    "svix-timestamp": svixIdTimeStamp,
+    "svix-signature": svixSignature,
   };
   const wh = new Webhook(webhookSecret);
   let evt: Event | null = null;
-
   try {
-    evt = wh.verify(
-      JSON.stringify(payload),
-      heads as IncomingHttpHeaders & WebhookRequiredHeaders
-    ) as Event;
-  } catch (err) {
-    console.error((err as Error).message);
-    return NextResponse.json({}, { status: 400 });
+    evt = wh.verify(payloadString, svixHeaders) as Event;
+  } catch (_) {
+    console.log("error");
+    return new Response("Error occured", {
+      status: 400,
+    });
   }
-
+  // Handle the webhook
   const eventType: EventType = evt.type;
   if (eventType === "user.created" || eventType === "user.updated") {
     const { id, firstName, username, imageUrl } = evt.data;
@@ -56,20 +61,15 @@ async function handler(request: Request) {
       });
     }
   }
-
   return new Response("", {
     status: 201,
   });
 }
 
-type EventType = "user.created" | "user.updated" | "*";
-
 type Event = {
-  data: User;
+  data: UserInterface;
   object: "event";
   type: EventType;
 };
 
-export const GET = handler;
-export const POST = handler;
-export const PUT = handler;
+type EventType = "user.created" | "user.updated" | "*";
